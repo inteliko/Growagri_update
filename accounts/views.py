1fb877f5a83be77945a97048729e5e2bc9776390
inteliko <rmaksusbinislam@gmail.com>
2024-04-19 18:04:35 +0800

    email_activate

--- accounts/views.py
+++ accounts/views.py
@@ -6,13 +6,15 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.contrib.auth import authenticate, login as auth_login 
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 
 #Email Verification 
 
 from django.template.loader import render_to_string
 from django.contrib.sites.shortcuts import get_current_site
-from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
@@ -49,8 +51,8 @@ def register(request):
             send_email = EmailMessage(mail_subject, message, to=[to_email])
             send_email.send()
 
-            messages.success(request, 'Registration Successful')
-            return redirect('register')
+            #messages.success(request, 'Registration Successful')
+            return redirect('accounts/login/?command=verification&email='+email)
 
     else: 
             form = RegistrationForm
@@ -98,5 +100,19 @@ def dashboard(request):
     return render(request, 'dashboard/dashboard.html')
 
 
-def activate(request):
-    return
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'COngratulations! Your Account is activate')
+        return redirect('login')
+    else:
+        messages.error(request, 'Invalid activation link')
+        return redirect('register')
+        
