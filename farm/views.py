43589726e594ba081a6666300a1643fc9641b0fa
inteliko <rmaksusbinislam@gmail.com>
2024-04-21 05:17:37 +0800

    add

--- farm/views.py
+++ farm/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 from .models import Farm
 from projects.models import CropType
+from carts.views import _cart_id, CartItem
 
 # Create your views here.
 
@@ -29,6 +29,8 @@ def farm(request, crop_slug=None):
 def farm_detail(request, crop_slug, farm_slug):
     try:
         single_product = Farm.objects.get(category__slug=crop_slug, slug= farm_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+        
     except Exception as e:
         raise e 
     
@@ -38,5 +40,6 @@ def farm_detail(request, crop_slug, farm_slug):
         'farm': farm,
         'total_sum_value': total_sum_value,
         'single_product': single_product,
+        'in_cart': in_cart,
     }
     return render(request, 'farm/farm_detail.html', context)
