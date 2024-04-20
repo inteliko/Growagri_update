619d1655375f9edd0342c578a164baa187b26aaa
inteliko <rmaksusbinislam@gmail.com>
2024-04-20 18:24:00 +0800

    add

--- carts/views.py
+++ carts/views.py
@@ -3,6 +3,8 @@ from farm.models import Farm
 from .models import Cart, CartItem
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect
+from django.core.exceptions import ObjectDoesNotExist
+
 
 
 
@@ -37,13 +39,47 @@ def add_cart(request, product_id):
 
         cart_item.save()
     
-    return HttpResponse(cart_item.product)
+   
     
+    return redirect('cart')
+
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Farm, id=product_id)  # Use Product instead of product
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Farm, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
     return redirect('cart')
 
     
 
 
 
-def cart(request):
-    return render(request, 'farm/cart.html')
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+
+        
+    except ObjectDoesNotExist:
+        pass
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items, 
+    }
+
+    return render(request, 'farm/cart.html', context)
