7400c7970011d46eaecd1eb2e0bcaaeec04b2b0e
inteliko <rmaksusbinislam@gmail.com>
2024-04-22 23:58:00 +0800

    add

--- funds/views.py
+++ funds/views.py
@@ -6,6 +6,11 @@ from .models import Order
 import datetime
 # Create your views here.
 
+
+def payments(request):
+    return render(request, 'funds/payments.html')
+
+
 def place_order(request, total=0, quantity=0, ):
     current_user = request.user
 
@@ -49,7 +54,14 @@ def place_order(request, total=0, quantity=0, ):
             order_number = current_date + str(data.id)
             data.order_number = order_number
             data.save()
-            return redirect('checkout')
+
+            order = Order.objects.get(user=current_user, is_ordered = False, order_number=order_number)
+            context = {
+                'order': order, 
+                'cart_items' : cart_items, 
+                'total': total,
+            }
+            return render(request, 'funds/payments.html', context)
         
         else:
             return redirect('checkout')
