800f6f9c347a12ed3aa803e91e02271a41890c73
inteliko <rmaksusbinislam@gmail.com>
2024-04-25 14:46:05 +0800

    add

--- weather/urls.py
+++ weather/urls.py
@@ -2,6 +2,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.weather_view, name='weather'),  # Main weather page
+    
+
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,30 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 import requests
 import datetime
 
 
 def weather_view(request):
-
     if request.method == 'POST':
-        city = request.POST.get('city', 'indore')  # Default to 'indore' if no city is provided
-        api_key = 'e3aff9dc246a6d6567ce7db04a8f4249'
-        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
-        
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            weather_data = {
-                'city': city,
-                'temperature': data['main']['temp'],
-                'description': data['weather'][0]['description'],
-                'icon': data['weather'][0]['icon']
-            }
-            return render(request, 'weather/weather.html', {'weather_data': weather_data})
-        else:
-            error_message = f'Error fetching weather data: {response.status_code}'
-            return render(request, 'weather/error.html', {'error_message': error_message})
+        city = request.POST.get('city', 'indore')
     else:
-        return render(request, 'weather/weather.html')
-               
-    
+        city = 'indore'
+
+    api_key = 'e3aff9dc246a6d6567ce7db04a8f4249'
+    url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric'
+
+    response = requests.get(url)
+    data = response.json()
+
+    if response.status_code == 200:
+        forecast_data = []
+        for item in data['list']:
+            # Parse the datetime string into a datetime object
+            datetime_str = item['dt_txt']
+            forecast_datetime = datetime.datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
+
+            forecast_data.append({
+                'datetime': forecast_datetime,
+                'temperature': item['main']['temp'],
+                'description': item['weather'][0]['description'],
+                'icon': item['weather'][0]['icon']
+            })
+
+        return render(request, 'dashboard/weather.html', {'forecast_data': forecast_data, 'city': city})
+    else:
+        error_message = data.get('message', 'Unknown error')
+        messages.error(request, f'Error: {error_message}')
+        return redirect('weather')
