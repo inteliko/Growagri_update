2d1338041ec42741c5c4010f5753163b5678c752
inteliko <rmaksusbinislam@gmail.com>
2024-04-30 02:44:36 +0600

    add

--- carts/views.py
+++ carts/views.py
@@ -71,25 +71,25 @@ def add_cart(request, product_id):
 
 
 
-def remove_cart(request, product_id):
-    product = get_object_or_404(Farm, id=product_id)  # Use Product instead of product
+def remove_cart(request, product_id, cart_item_id):
+    product = get_object_or_404(Farm, id=product_id)
     try:
-
         if request.user.is_authenticated:
-            cart_item = CartItem.objects.get(product=product, user=request.users)
+            cart_item = CartItem.objects.get(product=product, user=request.user, id=cart_item_id)
         else:
             cart = Cart.objects.get(cart_id=_cart_id(request)) 
-            cart_item = CartItem.objects.get(product=product, cart=cart)
+            cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
 
         if cart_item.quantity > 1:
             cart_item.quantity -= 1
             cart_item.save()
-    
         else:
             cart_item.delete()
-    except:
-        pass
+    except CartItem.DoesNotExist:
+        pass  # Handle the case where the cart item does not exist
+
     return redirect('cart')
+
 
 
 def remove_cart_item(request, product_id):
